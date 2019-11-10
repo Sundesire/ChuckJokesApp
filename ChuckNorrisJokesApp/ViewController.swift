10bc0423b24b175ebc92dab5b9c934eb8f60c11a
Ivan Babushkin <zet6666@gmail.com>
2019-11-10 18:35:08 +0300

    Initial Commit

---
+++ ChuckNorrisJokesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokesApp
+//
+//  Created by Иван Бабушкин on 10.11.2019.
+//  Copyright © 2019 Ivan Babushkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
